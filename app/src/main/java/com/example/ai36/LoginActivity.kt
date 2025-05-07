da670655b93cf4ca3ff5d2ef91e03b566d140f66
Jashmina <jashminathapa0@gmail.com>
2025-05-07 09:56:39 +0545

    commit

--- app/src/main/java/com/example/ai36/LoginActivity.kt
+++ app/src/main/java/com/example/ai36/LoginActivity.kt
@@ -7,13 +7,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
@@ -21,6 +24,9 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxColors
+import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
@@ -63,6 +69,10 @@ fun LoginBody(innerPadding: PaddingValues) {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var passwordVisibility by remember {
+        mutableStateOf(false)
+    }
+
+    var rememberMe by remember{
         mutableStateOf(false)
     }
 
@@ -165,7 +175,81 @@ fun LoginBody(innerPadding: PaddingValues) {
                 )
 
         )
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 8.dp, bottom = 8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Checkbox(
+                    checked = rememberMe,
+                    onCheckedChange = { rememberMe = it },
+                    colors = CheckboxDefaults.colors(
+                        checkedColor = Color.Green,
+                        checkmarkColor = Color.White
+                    )
+                )
+                Text(text = "Remember Me?")
+            }
+
+            Text(
+                text = "Forget Password?",
+                color = Color.Black,
+                modifier = Modifier.clickable {
+
+                }
+            )
+
+        }
+        Spacer(modifier = Modifier.height(30.dp))
+        Button(onClick = {
+
+        },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Login")
+        }
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Row (modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center ){
+                Text("New User?")
+                Text("Register here")
+            }
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(1.dp)
+                .background(Color.Gray)
+                .padding(top = 24.dp, bottom = 16.dp)
+        ) {}
+
+        Row {
+            Image(
+                painter = painterResource(id = R.drawable.facebook),
+                contentDescription = "Facebook Login",
+                modifier = Modifier
+                    .size(48.dp)
+                    .clickable{}
+            )
+            Image(
+                painter = painterResource(id = R.drawable.google),
+                contentDescription = "Facebook Login",
+                modifier = Modifier
+                    .size(48.dp)
+                    .clickable {}
+            )
+        }
+
     }
+
 }
 
 
